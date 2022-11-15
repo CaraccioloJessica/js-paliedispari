8e06da42cf7d9ae0a207e73ea7ea72430eabcdc4
Jessica <jesscara.993@gmail.com>
2022-11-15 16:02:37 +0100

    insert prompt and comments ex

--- pariedispari/js/main.js
+++ pariedispari/js/main.js
@@ -1,1 +1,14 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione). Dichiariamo chi ha vinto.
+
+// PROMPT DI SCELTA INIZIALI
+const scelta = prompt("Scegli pari o dispari");
+const numUtente = parseInt(prompt("Scegli un numero da 1 a 5"));
+
+// GENERARE NUMERO RANDOM PC DA 1 A 5
+
+// SOMMARE I DUE NUMERI
+
+// STABILIRE SE SOMMA E' PARI O DISPARI
+
+// OUTPUT VINCITORE
+
