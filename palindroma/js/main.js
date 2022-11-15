c4159862698c421e7aece9f5536165970c8b7ef9
Jessica <jesscara.993@gmail.com>
2022-11-15 15:10:07 +0100

    insert prompt that ask the user for a word

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,1 +1,10 @@
 // Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma.Dare output relativo.
+
+
+// CHIEDERE PAROLA AD UTENTE
+const parolaUtente = prompt('Inserisci una parola');
+
+console.log("La tua parola è:" + " " + parolaUtente);
+
+// VERIFICARE SE LA PAROLA E' PALINDROMA
+
